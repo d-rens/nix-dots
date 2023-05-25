6da219141dd025befe50ee94db9551e5e7204b3c
d-rens <displscord@gmail.com>
2023-05-25 22:12:33 +0200

    initial commit mess

---
+++ inkscape-figures/README.md
@@ -0,0 +1,4 @@
+# notice
+excuse the mess, now it finally works tho
+
+didnt with `pip instal...`, but it did with just sudoing the setup.py
---
+++ inkscape-figures/setup.py
@@ -0,0 +1,41 @@
+import pathlib
+from setuptools import setup, find_packages
+from distutils.spawn import find_executable
+import platform
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+dependencies = ['pyperclip', 'click', 'appdirs', 'daemonize']
+if find_executable("fswatch") is None:
+    if platform.system() == "Linux":
+        dependencies.append("inotify")
+    else:
+        raise ValueError(
+                "inkscape-figures needs fswatch to run on MacOS. You "
+                "can install it using `brew install fswatch`"
+                )
+
+setup(
+    name="inkscape-figures",
+    version="1.0.7",
+    description="Script for managing inkscape figures",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/gillescastel/inkscape-figures",
+    author="Gilles Castel",
+    author_email="gilles@castel.dev",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=['inkscapefigures'],
+    scripts=['bin/inkscape-figures'],
+    install_requires=dependencies,
+    include_package_data=True
+)
