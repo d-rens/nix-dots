5838b2f67d96711473cc141cbe9e319d3c822aaa
d-rens <displscord@gmail.com>
2023-07-22 21:02:52 +0200

    resolved keybind clash

--- nvim/lua/folder/remap.lua
+++ nvim/lua/folder/remap.lua
@@ -24,7 +24,7 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 
 -- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
+--vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
