2e234171bd5ac7dce47fe93dad854ee16ddb043c
d-rens <displscord@gmail.com>
2023-07-23 17:59:51 +0200

    substituted x symlinks to files

--- start-apps.sh
+++ start-apps.sh
@@ -1,1 +1,12 @@
-/home/daniel/start-apps.sh
+# here are things i for some reason dont put into my .xinitrc
+
+[[ -f ~/.Xmodmap ]] && xmodmap ~/.Xmodmap
+xmodmap -e 'clear Lock'
+~/.fehbg
+polybar &
+picom & 
+dunst &
+syncthing &
+python3 ~/tools/02_figures/inkscapefigures/main.py &
+python3 ~/tools/03_shortcuts/main.py &
+echo "no course selected" > /tmp/current_course
