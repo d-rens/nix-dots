b5aedbebcf81b84094234142339c5b2fb098634c
d-rens <displscord@gmail.com>
2023-07-26 16:39:15 +0200

    changed something, also changed it back

--- nvim/lua/folder/packer.lua
+++ nvim/lua/folder/packer.lua
@@ -62,22 +62,22 @@ return require('packer').startup(function(use)
       use('humanoid-colors/vim-humanoid-colorscheme')
       --use('github/copilot.vim')
       use('windwp/nvim-autopairs')
-      use{
-         'VonHeikemen/lsp-zero.nvim',
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-nvim-lua'},
-      -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
+
+      use{ 'VonHeikemen/lsp-zero.nvim',
+          -- LSP Support
+          {'neovim/nvim-lspconfig'},
+          {'williamboman/mason.nvim'},
+          {'williamboman/mason-lspconfig.nvim'},
+          -- Autocompletion
+          {'hrsh7th/nvim-cmp'},
+          {'hrsh7th/cmp-buffer'},
+          {'hrsh7th/cmp-path'},
+          {'saadparwaiz1/cmp_luasnip'},
+          {'hrsh7th/cmp-nvim-lsp'},
+          {'hrsh7th/cmp-nvim-lua'},
+          -- Snippets
+          {'L3MON4D3/LuaSnip'},
+          {'rafamadriz/friendly-snippets'},
       }
 
 end)
