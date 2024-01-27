1382fcd01c2c7de0de31c42d1fd4f562918c0698
d-rens <d.renschler@icloud.com>
2024-01-27 16:51:42 +0100

    Changed lsp filename

--- nvim/lua/d-rens/plugins/lsp.lua
+++ nvim/lua/d-rens/plugins/lsp.lua
@@ -4,17 +4,15 @@ return {
   dependencies = {
     "SirVer/ultisnips",
     "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-cmdline",
     "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-path",
     "hrsh7th/cmp-path",
     "hrsh7th/nvim-cmp",
     "micangl/cmp-vimtex",
     "neovim/nvim-lspconfig",
     "quangnguyen30192/cmp-nvim-ultisnips",
   },
+
   opts = function()
     vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
     local cmp = require("cmp")
@@ -26,9 +24,9 @@ return {
       mapping = cmp.mapping.preset.insert({
         ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
+        --["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        --["<C-f>"] = cmp.mapping.scroll_docs(4),
+        --["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         ["<S-CR>"] = cmp.mapping.confirm({
