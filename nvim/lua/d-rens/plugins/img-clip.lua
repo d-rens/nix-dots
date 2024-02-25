b0ac0189bffe94fb614ffe2636eb92ebfb3f64c9
d-rens <d-rens@mailbox.org>
2024-02-25 18:31:34 +0100

    Added clipboard image paste plugin

---
+++ nvim/lua/d-rens/plugins/img-clip.lua
@@ -0,0 +1,23 @@
+return {
+    "HakonHarnes/img-clip.nvim",
+    event = "BufEnter",
+    opts = {
+        filetypes = {
+            tex = {
+relative_template_path = false,
+template = [[
+\begin{figure}
+\centering
+\includegraphics[width=0.8\linewidth]{$FILE_PATH}
+\caption{$CURSOR}
+\label{fig:$LABEL}
+\end{figure}
+]],
+            },
+        },
+    },
+    keys = {
+        -- suggested keymap
+        { "<leader>P", "<cmd>PasteImage<cr>", desc = "Paste clipboard image" },
+    },
+}
