d3940d9a206dbd6f2bfc6cf1f81644074a84a98e
d-rens <d-rens@mailbox.org>
2024-04-04 13:23:15 +0200

    minor changes

--- nvim/lua/d-rens/plugins/snippets.lua
+++ nvim/lua/d-rens/plugins/snippets.lua
@@ -1,7 +1,7 @@
 return {
         "SirVer/ultisnips",
         keys = {
-            {"<tab>", vim.cmd.UltiSnipsExpandTrigger},
+            --{"<C-n>", vim.cmd.UltiSnipsExpandTrigger},
             {"<C-u>", vim.cmd.UltiSnipsEdit},
             {"<C-n>", vim.cmd.UltiSnipsJumpForwardTrigger,
             {"<C-b>", vim.cmd.UltiSnipsJumpBackwardTrigger},
