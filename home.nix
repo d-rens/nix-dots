2c6858b05aed49ff60137e7fe75e7384edf2081a
d-rens <daniel@d-rens.xyz>
2024-05-29 10:18:37 +0200

    Added neovim as git editor

--- home.nix
+++ home.nix
@@ -42,6 +42,7 @@
         csa = "commit -m -S --ammend";
       };
     extraConfig = {
+      core.editor = "nvim";
       init.defaultBranch = "main";
       commit.gpgsign = true;
       user.signingkey = "F0F708E2ED281B48DF26A213DCF60F08282ADCB9";
