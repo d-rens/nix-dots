c868b233da145ecd2a5d9e74dd451bc2f0267cfd
d-rens <daniel@d-rens.xyz>
2024-06-16 19:04:58 +0200

    Deleted old neovim config

--- home/common/core/old-neovim/src/lua/d-rens/init.lua
+++
@@ -1,3 +0,0 @@
-require("d-rens.lazy.options")
-require("d-rens.lazy.keymaps")
-require("d-rens.lazy")
--- home/common/core/old-neovim/src/lua/d-rens/plugins/todo.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-        {"<leader>ts", "<cmd>TodoTelescope<CR>"}
-    }
-}
-
-
