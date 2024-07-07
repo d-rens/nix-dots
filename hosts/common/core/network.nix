e7a590410f7bc8d3a32ad193b4e29e82e4763dfd
d-rens <daniel@d-rens.xyz>
2024-07-07 20:29:32 +0200

    Adds wifi config

--- hosts/common/core/network.nix
+++ hosts/common/core/network.nix
@@ -1,7 +1,45 @@
-{ config, pkgs, inputs, ... }:
-
+{ 
+  config,
+  ... 
+}:
+################################ HAVE NOT CONFIRMED IF IT WORKS ###############################
+################################ HAVE NOT CONFIRMED IF IT WORKS ###############################
+################################ HAVE NOT CONFIRMED IF IT WORKS ###############################
+################################ HAVE NOT CONFIRMED IF IT WORKS ###############################
 {
+  sops.secrets = {
+    "wifi/psk" = {
+      owner = config.users.users.da.name;
+      inherit (config.users.users.da) group;
+    };
+  };
   networking = {
-    networkmanager.enable = true;
+    networkmanager = {
+      enable = true;
+      ensureProfiles.profiles = {
+        home-wifi = {
+          connection = {
+            id = "home";
+            permissions = "";
+            type = "wifi";
+          };
+          ipv4 = {
+            method = "auto";
+          };
+          ipv6 = {
+            addr-gen-mode = "stable-privacy";
+            method = "auto";
+          };
+          wifi = {
+            ssid = "DuKommstHierNetRein";
+          };
+          wifi-security = {
+            auth-alg = "open";
+            key-mgmt = "wpa-psk";
+            psk = "${config.sops.secrets."wifi/psk".path}";
+          };
+        };
+      };
+    };
   };
 }
