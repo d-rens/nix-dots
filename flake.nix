c8dfc6504bebfc63d5cb0ac1c2796770f8a15da5
d-rens <daniel@d-rens.xyz>
2024-07-14 08:51:01 +0200

    Switches nixpkgs source back to unstable

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "flake config";
 
   inputs = {
-    #nixpkgs.url = "nixpkgs/nixos-unstable";
-    nixpkgs.url = "nixpkgs/master";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    #nixpkgs.url = "nixpkgs/master";
 
     home-manager = {
       url = "github:nix-community/home-manager";
