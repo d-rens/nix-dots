c6e3458598b6810a141184d523a48ff2caeab60f
d-rens <daniel@d-rens.xyz>
2024-07-14 17:51:20 +0200

    Adds option to auto-genarte age-key

--- hosts/common/core/sops.nix
+++ hosts/common/core/sops.nix
@@ -1,12 +1,14 @@
 { ... }:
 {
-
   sops = {
     defaultSopsFile = ../../../secrets.yaml;
     defaultSopsFormat = "yaml";
 
     age = {
       keyFile = "/home/da/.config/sops/age/keys.txt";
+      sshKeyPaths = [
+        "/etc/ssh/ssh_host_ed25519_key"
+      ];
     };
 
     secrets = {
