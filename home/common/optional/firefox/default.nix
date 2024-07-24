2948f27fab6345b3b54b081eeb82eb1d99128153
d-rens <daniel@d-rens.xyz>
2024-07-24 12:23:49 +0200

    Adds more firefox about:config settings

--- home/common/optional/firefox/default.nix
+++ home/common/optional/firefox/default.nix
@@ -26,13 +26,21 @@
       settings = {
         "browser.activity-stream.showSponsored" = false;
         "browser.activity-stream.showSponsoredTopSites" = false;
+        "browser.bookmarks.addedImportButton" = false;
+        "browser.compactmode.show" = true;
         "browser.download.panel.shown" = true;
         "browser.newtabpage.activity-stream.default.sites" = "https://github.com, https://test.test" ;
+        "browser.newtabpage.activity-stream.discoverystream.sendToPocket.enabled" = false;
         "browser.newtabpage.activity-stream.system.showSponsored" = false;
-        "browser.startup.homepage" = "https://www.startpage.com/do/mypage.pl?prfe=223d5083f09bc6377ea5906987b218cb912b068bc5e8b6d68952d4f0cf7273eae18b8ff358b5c1d2dc134354935b3da83071a514a26b501a644968376a65d1e5c7304ef4a703ef74b22ee107";
         "browser.startup.blankWindow" = true;
+        "browser.startup.homepage" = "https://www.startpage.com/do/mypage.pl?prfe=223d5083f09bc6377ea5906987b218cb912b068bc5e8b6d68952d4f0cf7273eae18b8ff358b5c1d2dc134354935b3da83071a514a26b501a644968376a65d1e5c7304ef4a703ef74b22ee107";
+        "browser.theme.toolbar-theme" = 2;
+        "browser.toolbars.bookmarks.visibility" = "newtab";
+        "browser.uidensity" = 1;
+        "browser.urlbar.suggest.pocket" = false;
         "dom.security.https_only_mode" = true;
         "extensions.packet.showHome" = false;
+        "extensions.pocket.enabled" = false;
         "extensions.quarantinedDomains" = false;
         "identity.fxaccounts.enabled" = false;
         "services.sync.prefs.sync.browser.newtabpage.activity-stream.showSponsored" = false;
@@ -41,8 +49,6 @@
         "signon.rememberSignons" = false;
         "startup.homepage_welcome_url" = "www.startpage.com";
         "ui.key.menuAccessKeyFocuses" = false;
-        "browser.compactmode.show" = true;
-        "browser.uidensity" = 1;
       };
 
       extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
