ffbb3525eec46ca12d2a26deddcb5a792ad83a09
d-rens <daniel@d-rens.xyz>
2024-07-24 18:57:41 +0200

    Added minimal borg setup

---
+++ hosts/common/optional/borg.nix
@@ -0,0 +1,33 @@
+{
+  ...
+}:
+{
+  #sops.secrets = {
+  #  "borg-key" = {
+  #    owner = config.users.users.da.name;
+  #    inherit (config.users.users.da) group;
+  #  };
+  #};
+
+  services.borgbackup.jobs.t470 = {
+    paths = "/home/da/";
+
+    environment = {
+      BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK = "yes";
+      BORG_RSH = "ssh -o 'StrictHostKeyChecking=no' -i /home/da/.ssh/id_ed25519";
+    };
+
+    exclude = [
+      "/home/da/.*"
+      "/home/da/Downloads/"
+    ];
+
+    encryption = {
+      mode = "none";
+    };
+
+    repo = "ssh://pi@192.168.0.137:22/mnt/data/borg/t470";
+    compression = "zstd,1";
+    startAt = "hourly";
+  };
+}
