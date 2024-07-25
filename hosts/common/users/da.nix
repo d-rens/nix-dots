1402f384858f414ec248aa4bc4eefd6f6d3a0ccd
d-rens <daniel@d-rens.xyz>
2024-07-25 12:24:19 +0200

    Adds ssh authKey to root user
    This will be used when installing with disko remotely to not log oneself
    out of the system.

--- hosts/common/users/da.nix
+++ hosts/common/users/da.nix
@@ -34,6 +34,9 @@
         "scanner" 
       ];
       initialPassword = "password"; 
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOS9NK7X0K5mgoSTlAB+v8QlIDynJDJCPo6QO3IamnFu da@nixos-t"
+      ];
     };
   };
 }
